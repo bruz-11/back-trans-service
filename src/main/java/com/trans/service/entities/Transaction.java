1b39b2444354f1b0200c6d29ff531da41c7fa8f8
bruz-11 <b.ruz@duocuc.cl>
2026-09-16 01:03:06 -0300

    feat: crear entidad Transaction y mapeo de tabla con JPA

---
+++ src/main/java/com/trans/service/entities/Transaction.java
@@ -0,0 +1,52 @@
+package com.trans.service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "transferencias")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTransferencia;
+
+    @Column (name = "id_usuario", nullable = false)
+    private Long idUsuario;
+
+    @Column (name = "rut_origen", nullable = false)
+    private String rutOrigen;
+
+    @Column (name = "id_contacto", nullable = false)
+    private Long idContacto;
+
+    @Column (name = "rut_destino", nullable = false)
+    private String rutDestino;
+
+    @Column (name = "monto", nullable = false)
+    private BigDecimal monto;
+
+    @Column (name = "fecha_transferencia", nullable = false)
+    private LocalDateTime fechaTransferencia;
+
+    @Column (name = "estado", nullable = false)
+    private String estado;
+}
