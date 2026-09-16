778f0a927192f298c43f93df8fc1afeecb96c8ab
bruz-11 <b.ruz@duocuc.cl>
2026-09-16 01:28:05 -0300

    feat(repo): agregar metodo findByIdUsuario en repository

--- src/main/java/com/trans/service/repositories/TransactionRepository.java
+++ src/main/java/com/trans/service/repositories/TransactionRepository.java
@@ -1,10 +1,13 @@
 package com.trans.service.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.trans.service.entities.Transaction;
 
 @Repository 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByIdUsuario(Long idUsuario);
 
 }
