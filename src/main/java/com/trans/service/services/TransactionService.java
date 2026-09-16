a463be4c7282c4c5c92190bb4093e3dd1c92bc3f
bruz-11 <b.ruz@duocuc.cl>
2026-09-16 01:10:04 -0300

    feat(service): actualizar logica y formato en TransactionService

--- src/main/java/com/trans/service/services/TransactionService.java
+++ src/main/java/com/trans/service/services/TransactionService.java
@@ -1,5 +1,30 @@
 package com.trans.service.services;
 
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+import com.trans.service.repositories.TransactionRepository;
+import com.trans.service.entities.Transaction;
+import com.trans.service.dto.TransactionRequestDto;
+import java.time.LocalDateTime;
+
+    
+@Service 
+@RequiredArgsConstructor
 public class TransactionService {
 
+    private final TransactionRepository transactionRepository;
+
+    public Transaction transferir(TransactionRequestDto dto) {
+        Transaction transaction = new Transaction();
+        transaction.setIdUsuario(dto.getIdUsuario());
+        transaction.setRutOrigen(dto.getRutOrigen());
+        transaction.setIdContacto(dto.getIdContacto());
+        transaction.setRutDestino(dto.getRutDestino());
+        transaction.setMonto(dto.getMonto());
+        transaction.setFechaTransferencia(LocalDateTime.now());
+        transaction.setEstado("PENDIENTE");
+
+        return transactionRepository.save(transaction);
+    }
 }
