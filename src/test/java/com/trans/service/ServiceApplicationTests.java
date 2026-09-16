01e15d5bdc978abe4b99e73a6a6131be87e72a63
bruz-11 <b.ruz@duocuc.cl>
2026-09-16 00:00:31 -0300

    feat: commit inicial microservicio transacciones

---
+++ src/test/java/com/trans/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trans.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
